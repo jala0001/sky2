24125c9701d065427d89b93181a29eb286df314e
Jamie <jamiehye@live.dk>
2024-04-08 14:53:56 +0200

    virk

---
+++ src/test/java/com/example/sky2/Sky2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sky2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sky2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
